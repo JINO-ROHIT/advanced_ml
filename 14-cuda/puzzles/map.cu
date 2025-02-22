10d6cf9e91271842bebcf09761c50841bcc2df22
JINO-ROHIT <find.jinorohit@gmail.com>
2025-02-22 14:45:15 +0530

    puzzles

---
+++ 14-cuda/puzzles/map.cu
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+const int DSIZE = 1024;
+
+__global__ void map(float *a, int ds){
+    int idx = threadIdx.x;
+    if (idx < ds){
+        a[idx] += 10;
+    }
+}
+
+int main(){
+    float *h_A;
+    float *d_A;
+
+    h_A = new float[DSIZE];
+
+    for (int i = 0; i < DSIZE; i++)
+    {
+        h_A[i] = rand() / (float) RAND_MAX;
+    }
+
+
+    cudaMalloc(&d_A, DSIZE * sizeof(float));
+
+    cudaMemcpy(d_A, h_A, DSIZE * sizeof(float), cudaMemcpyHostToDevice);
+
+    map<<<1, DSIZE>>>(d_A, DSIZE);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(h_A, d_A, DSIZE * sizeof(float), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < 10; i++) {
+        printf("h_A[%d] = %f\n", i, h_A[i]);
+    }
+
+    cudaFree(d_A);
+
+    delete[] h_A;
+}
