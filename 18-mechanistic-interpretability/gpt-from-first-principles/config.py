9eb794d3d2ee4beac985e14ca9b61ad22e07b282
JINO-ROHIT <find.jinorohit@gmail.com>
2025-08-22 09:49:24 +0530

    setup

---
+++ 18-mechanistic-interpretability/gpt-from-first-principles/config.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    d_model: int = 768
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024 # the max sequence length
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+cfg = Config()
+print(cfg)
---
+++ 18-mechanistic-interpretability/gpt-from-first-principles/tests.py
@@ -0,0 +1,39 @@
+import torch
+from transformer_lens import HookedTransformer
+
+from config import Config
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def rand_float_test(cls, shape):
+    cfg = Config()
+    layer = cls(cfg).to(device)
+    random_input = torch.randn(shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def rand_int_test(cls, shape):
+    cfg = Config()
+    layer = cls(cfg).to(device)
+    random_input = torch.randint(100, 1000, shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def load_gpt2_test(cls, gpt2_layer, input):
+    cfg = Config()
+    layer = cls(cfg).to(device)
+    layer.load_state_dict(gpt2_layer.state_dict(), strict = False)
+    print("Input shape:", input.shape)
+    output = layer(input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape)
+    try: reference_output = gpt2_layer(input)
+    except: reference_output = gpt2_layer(input, input, input)
+    print("Reference output shape:", reference_output.shape, "\n")
+    comparison = torch.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum()/comparison.numel():.2%} of the values are correct\n")
