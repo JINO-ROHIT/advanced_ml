0ebcc03e194e5cdaa880565c3c28d5fc044489bb
JINO-ROHIT <find.jinorohit@gmail.com>
2025-08-23 14:25:16 +0530

    attn layer

---
+++ 18-mechanistic-interpretability/gpt-from-first-principles/attention.py
@@ -0,0 +1,90 @@
+import math
+from jaxtyping import Float, Int
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+from transformer_lens import HookedTransformer
+import einops
+
+from config import Config
+from tests import rand_float_test, load_gpt2_test
+
+reference_gpt2 = HookedTransformer.from_pretrained("gpt2-small", fold_ln = False, center_unembed = False, center_writing_weights = False)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class Attention(nn.Module):
+    IGNORE: Float[Tensor, ""]
+
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_Q = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_K = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_V = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_O = nn.Parameter(torch.empty((cfg.n_heads, cfg.d_head, cfg.d_model)))
+        self.b_Q = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_K = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_V = nn.Parameter(torch.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_O = nn.Parameter(torch.zeros((cfg.d_model)))
+        nn.init.normal_(self.W_Q, std = self.cfg.init_range)
+        nn.init.normal_(self.W_K, std = self.cfg.init_range)
+        nn.init.normal_(self.W_V, std = self.cfg.init_range)
+        nn.init.normal_(self.W_O, std = self.cfg.init_range)
+        self.register_buffer("IGNORE", torch.tensor(-1e5, dtype = torch.float32, device=device))
+
+    def forward(
+        self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        q = einops.einsum(
+            normalized_resid_pre, self.W_Q,
+            "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head",
+        ) + self.b_Q
+        k = einops.einsum(
+            normalized_resid_pre, self.W_K,
+            "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head",
+        ) + self.b_K
+        v = einops.einsum(
+            normalized_resid_pre, self.W_V,
+            "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head",
+        ) + self.b_V
+
+        attn_scores = einops.einsum(
+            q, k,
+            "batch posn_Q nheads d_head, batch posn_K nheads d_head -> batch nheads posn_Q posn_K",
+        )
+        attn_scores_masked = self.apply_causal_mask(attn_scores / self.cfg.d_head ** 0.5)
+        attn_pattern = attn_scores_masked.softmax(-1)
+
+
+        z = einops.einsum(
+            v, attn_pattern,
+            "batch posn_K nheads d_head, batch nheads posn_Q posn_K -> batch posn_Q nheads d_head",
+        )
+
+        attn_out = einops.einsum(
+            z, self.W_O,
+            "batch posn_Q nheads d_head, nheads d_head d_model -> batch posn_Q d_model",
+        ) + self.b_O
+
+        return attn_out
+
+
+
+    def apply_causal_mask(
+        self, attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"]
+    ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
+        '''
+        Applies a causal mask to attention scores, and returns masked scores.
+        '''
+        lower = torch.tril(torch.ones(attn_scores.size(-2), attn_scores.size(-1), device = device)).bool()
+        return torch.where(lower, attn_scores, self.IGNORE)
+
+reference_text = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+tokens = reference_gpt2.to_tokens(reference_text).to(device)
+logits, cache = reference_gpt2.run_with_cache(tokens)
+
+rand_float_test(Attention, [2, 4, 768])
+load_gpt2_test(Attention, reference_gpt2.blocks[0].attn, cache["normalized", 0, "ln1"])
