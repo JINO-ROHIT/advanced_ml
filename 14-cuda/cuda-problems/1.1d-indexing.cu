d6b1b1c60633f0b40039bbe25dfe0176bce4cad8
JINO-ROHIT <find.jinorohit@gmail.com>
2025-09-23 23:10:46 +0530

    1d indexing

---
+++ 14-cuda/cuda-problems/1.1d-indexing.cu
@@ -0,0 +1,55 @@
+// single block, 1024 threads
+// 1d indexing to element wise add.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+int DSIZE = 512;
+
+__global__ void add(float *A, float *B, float *C, int size){
+    int idx = threadIdx.x;
+    if(idx < size){
+        C[idx] = A[idx] + B[idx];
+    }
+}
+
+int main() {
+
+    float *h_A, *h_B, *h_C;
+    float *d_A, *d_B, *d_C;
+
+    h_A = new float[DSIZE];
+    h_B = new float[DSIZE];
+    h_C = new float[DSIZE];
+
+    for (int i = 0; i < DSIZE; i++) {
+        h_A[i] = rand() / (float)RAND_MAX;
+        h_B[i] = rand() / (float)RAND_MAX;
+    }
+
+    cudaMalloc(&d_A, sizeof(float) * DSIZE);
+    cudaMalloc(&d_B, sizeof(float) * DSIZE);
+    cudaMalloc(&d_C, sizeof(float) * DSIZE);
+
+    cudaMemcpy(d_A, h_A, sizeof(float) * DSIZE, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, sizeof(float) * DSIZE, cudaMemcpyHostToDevice);
+
+    add<<<1, DSIZE>>>(d_A, d_B, d_C, DSIZE);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(h_C, d_C, sizeof(float) * DSIZE, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < 10; i++) {
+        printf("C[%d] = %f \n", i, h_C[i]);
+    }
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
+}
